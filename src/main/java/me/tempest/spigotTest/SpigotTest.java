5734e1b5298d62dd7a41d2fdba840176f9070334
Tempest <krukovtempest@gmail.com>
2024-08-30 09:26:37 -0500

    Added basic events
    +Added an event for player joining
    +Added an event for playing leaving bed

--- src/main/java/me/tempest/spigotTest/SpigotTest.java
+++ src/main/java/me/tempest/spigotTest/SpigotTest.java
@@ -1,17 +1,40 @@
 package me.tempest.spigotTest;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SpigotTest extends JavaPlugin {
+// https://hub.spigotmc.org/javadocs/bukkit/
+
+public final class SpigotTest extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("First Plugin has started");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Plugin has shut down");
     }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
+        playerJoinEvent.setJoinMessage("Hello, " + playerJoinEvent.getPlayer().getName());
+        System.out.println("A player has joined");
+
+    }
+
+    @EventHandler
+    public void onPlayerLeaveBed(PlayerBedLeaveEvent event) {
+        String playerName = event.getPlayer().getName();
+        getServer().broadcastMessage("Go back to sleep " + playerName + ", the night is dark and full of terrors.");
+    }
+
+
 }
