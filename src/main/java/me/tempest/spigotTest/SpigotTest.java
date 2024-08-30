5145009fc7978be59f2f500b6cf6acc6e2a5a171
Tempest <krukovtempest@gmail.com>
2024-08-30 09:05:13 -0500

    Initial commit

---
+++ src/main/java/me/tempest/spigotTest/SpigotTest.java
@@ -0,0 +1,17 @@
+package me.tempest.spigotTest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
