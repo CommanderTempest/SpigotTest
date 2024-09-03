d5fc389e52951eaf9bb167d379c065d1d0e13485
Tempest <krukovtempest@gmail.com>
2024-09-03 14:28:33 -0500

    Added optional arguments
    +The God command now takes an optional argument (other player name), and will target that player with god

--- src/main/java/me/tempest/spigotTest/commands/GodCommand.java
+++ src/main/java/me/tempest/spigotTest/commands/GodCommand.java
@@ -1,5 +1,6 @@
 package me.tempest.spigotTest.commands;
 
+import me.tempest.spigotTest.SpigotTest;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,20 +10,45 @@ import org.bukkit.entity.Player;
 public class GodCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player player) {
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (args.length == 1)
+        {
+            // find if it matches player name
+            String playerName = args[0];
+            Player target = SpigotTest.getPlugin().getServer().getPlayerExact(playerName);
 
-            if (player.hasPermission("spigottest.god")) {
-                if (player.isInvulnerable()) {
-                    player.setInvulnerable(false);
-                    player.sendMessage(ChatColor.RED + "God mode has been disabled.");
-                } else {
-                    player.setInvulnerable(true);
-                    player.sendMessage(ChatColor.RED + "God mode has been enabled.");
-                }
+            if (target == null)
+            {
+                commandSender.sendMessage("Player is not online");
             }
+            else
+            {
+                setInvuln(target, commandSender);
+            }
+        }
+        else if (args.length == 0)
+        {
+            if (commandSender instanceof Player player) {
+                setInvuln(player, player);
+            }
+        }
+        else {
+            commandSender.sendMessage("Invalid number of arguments. /god <player>");
         }
 
         return true;
+    }
+
+    private void setInvuln(Player player, CommandSender sender)
+    {
+        if (sender.hasPermission("spigottest.god")) {
+            if (player.isInvulnerable()) {
+                player.setInvulnerable(false);
+                player.sendMessage(ChatColor.RED + "God mode has been disabled.");
+            } else {
+                player.setInvulnerable(true);
+                player.sendMessage(ChatColor.RED + "God mode has been enabled.");
+            }
+        }
     }
 }
