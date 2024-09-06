17c330427a2917d4c769fd66aba16fc5f28d2cf1
Tempest <krukovtempest@gmail.com>
2024-09-06 12:37:43 -0500

    MongoDB Practice
    +Added MongoDB Java Driver Dependency to pom.xml
    +Did basic practice with connecting, and inserting data into my mongoDB database

--- src/main/java/me/tempest/spigotTest/SpigotTest.java
+++ src/main/java/me/tempest/spigotTest/SpigotTest.java
@@ -1,7 +1,11 @@
 package me.tempest.spigotTest;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import me.tempest.spigotTest.commands.*;
 import me.tempest.spigotTest.listeners.*;
+import org.bson.Document;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -19,7 +23,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * (COMPLETED) Look up custom inventories, custom items, how to make, etc.
  * Look up custom entities
  * Take a look at how holograms are done
- * Look at MongoDB
+ * (COMPLETED) Look at MongoDB
  **/
 
 public final class SpigotTest extends JavaPlugin implements Listener {
@@ -32,6 +36,16 @@ public final class SpigotTest extends JavaPlugin implements Listener {
         // config.yml
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+
+        // MongoDB Connection
+        MongoClient mongoClient = MongoClients.create("mongodb+srv://krukovtempest:tEAjiygHEc25AoA4@spigotlearn.o0vh7.mongodb.net/?retryWrites=true&w=majority&appName=SpigotLearn");
+        MongoCollection<Document> collection = mongoClient.getDatabase("SpigotLearning").getCollection("users");
+
+        System.out.println("Connected to Database");
+        Document document1 = new Document("name", "Tempest")
+                .append("level", 105)
+                .append("kills", 42875);
+        collection.insertOne(document1);
 
         // Plugin startup logic
         System.out.println("First Plugin has started");
