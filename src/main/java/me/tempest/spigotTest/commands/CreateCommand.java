92c6ce62a4dbf32e354f3949763581091c93e87f
Tempest <krukovtempest@gmail.com>
2024-09-12 12:41:25 -0500

    Fixed imports
    +Fixed imports from yesterday
    
    #Will continue to work on NMS tomorrow

--- src/main/java/me/tempest/spigotTest/commands/CreateCommand.java
+++ src/main/java/me/tempest/spigotTest/commands/CreateCommand.java
@@ -1,9 +1,17 @@
 package me.tempest.spigotTest.commands;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerChatPacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_21_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -18,15 +26,16 @@ public class CreateCommand implements CommandExecutor {
             ServerPlayer sp = craftPlayer.getHandle();
 
             MinecraftServer server = sp.getServer();
-            ServerLevel level = sp.getLevel();
+            ServerLevel level = sp.serverLevel();
             GameProfile profile = new GameProfile(UUID.randomUUID(), "Tom");
 
-            ServerPlayer npc = new ServerPlayer(server, level, profile);
+            ServerPlayer npc = new ServerPlayer(server, level, profile, sp.clientInformation());
 
             ServerGamePacketListenerImpl ps = sp.connection;
 
-            ps.send(new ClientboundPlayerInfoPacket(Action.ADD_PLAYER, npc));
-            ps.send(new ClientboundAddPlayerPacket(sp));
+            // look into this at later date
+            ps.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, npc));
+            //ps.send(new ClientboundAddEntityPacket(sp));
         }
 
         return true;
