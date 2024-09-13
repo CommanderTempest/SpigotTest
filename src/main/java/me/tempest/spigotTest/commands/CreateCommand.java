3c6812282fe66055a22f1078928b21ef88b4c916
Tempest <krukovtempest@gmail.com>
2024-09-13 10:23:09 -0500

    NMS NPC now has Skin
    +Added a skin to the NPC

--- src/main/java/me/tempest/spigotTest/commands/CreateCommand.java
+++ src/main/java/me/tempest/spigotTest/commands/CreateCommand.java
@@ -1,19 +1,24 @@
 package me.tempest.spigotTest.commands;
 
 import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import me.tempest.spigotTest.SpigotTest;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
-import net.minecraft.network.protocol.game.ClientboundPlayerChatPacket;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_21_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
 import java.util.UUID;
 
 public class CreateCommand implements CommandExecutor {
@@ -30,14 +35,34 @@ public class CreateCommand implements CommandExecutor {
             GameProfile profile = new GameProfile(UUID.randomUUID(), "Tom");
 
             ServerPlayer npc = new ServerPlayer(server, level, profile, sp.clientInformation());
+            npc.setPos(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ());
+
+            String signature = "D0uWzO3V+VVvcOTQgP6JRIAbqtkyDwbBr2pYwy44XSr5mGftXn9IwHHnQZ5/3aXFUH3m1ldTZ7Gjtf2RXkTPxZ45h7WOSYE/sksdyMff0s/HNCR5UmbkbTveioT+irvy1Lw++1a2aNdKa+Hp61KUw14byd5Bmsiv1rf+ULW9NIcQmUKi0fQZwZzGZO12w8Kzmyygj6dEcz6VaCxHDJpyAKt0mQFlql1ANOcCJ7zB5hiHHTW/F72jfORZWq3nLikVccG4yYgZH7Lsfkzvc0PRgccNHVz6LDS519fljEi3N9hdq1AhwKLiMoxDFbRFI7aLVTuY6yX9+dQLdlWx2xsk3KPeslK1smXHbuauO9cfOr60TyhgW60PrvhxvH0pV4BlO9r5PbUFxszyGmos7VB2cINlf+iol7tl9tYWeLhmsW7Fmx8uN02p8rMIinaJahx6m4w8GW4NWY4snkDjC9AUSeMpLCd5GzChflf0EQQCyDoBDyrVLiMM0L/yTfgg2ba3KWmsYVqtTu0lPJDyihNpk9YoBpAgHAkq4/NxdHk9nXksrXWT/3JLY4JY2oEP1wkHrWX3bXVyuQbBBA3oT5imXf73CwO6Whe3nWPGkjESDaHK5WPLXeWkTiJ7RozMigTKu6c+MYLlNh/s4JaqpjY1XMiKnLDw2Jpu5gfTXI+YCpg=";
+            String texture = "ewogICJ0aW1lc3RhbXAiIDogMTcyNjI0MDcwODMyNSwKICAicHJvZmlsZUlkIiA6ICIwNjkxOGIxNjVhNjQ0MjdlYjE3N2Y2YmUwYmViYjMzMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJTYWx0ZWRfIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2RmODFlNTY2YTQ5ZTk0MzczM2QxYmRkMjY5MDM2MzVkYmNjZGU2ZjdiMDMyMjE5YjYzNzZjZjU2ZWYyZjFiMTAiCiAgICB9CiAgfQp9";
+
+            profile.getProperties().put("textures", new Property("textures", texture, signature));
+
+            SynchedEntityData synchedEntityData = npc.getEntityData();
 
             ServerGamePacketListenerImpl ps = sp.connection;
 
-            // look into this at later date
             ps.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, npc));
+
+            //sendPacket(new ClientboundAddEntityPacket(npc), player);
+            sendPacket(new ClientboundSetEntityDataPacket(npc.getId(), synchedEntityData.getNonDefaultValues()), player);
+            Bukkit.getScheduler().runTaskLaterAsynchronously(SpigotTest.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    sendPacket(new ClientboundPlayerInfoRemovePacket(Collections.singletonList(npc.getUUID())), player);
+                }
+            }, 40);
             //ps.send(new ClientboundAddEntityPacket(sp));
+            //ps.send(new ClientboundAddEntityPacket(npc));
         }
 
         return true;
+    }
+    public void sendPacket(Packet<?> packet, Player player) {
+        ((CraftPlayer) player).getHandle().connection.send(packet);
     }
 }
